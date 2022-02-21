24fb86c8b8c576f20765c82f3c4c99a95048e1af
許煜廷 <me@hsuyuting.com>
2022-02-22 02:24:44 +0800

    Update config

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -2,7 +2,12 @@ lsp = require "lspconfig"
 lspfuzzy = require "lspfuzzy"
 
 lspfuzzy.setup {jump_one = false}
-lsp.svelte.setup {}
-lsp.tsserver.setup {}
+
 lsp.gopls.setup {}
+lsp.sumneko_lua.setup{}
+lsp.html.setup {}
+lsp.jsonls.setup{}
+lsp.tsserver.setup {}
+lsp.svelte.setup {}
+
 vim.api.nvim_buf_set_option(0, "omnifunc", "v:lua.vim.lsp.omnifunc")
