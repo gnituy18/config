889e0e3b200fc56461c64084f5b4d2f91ffaf025
gnituy18 <you@hsuyuting.com>
2022-06-25 15:13:36 +0800

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,20 +14,15 @@ vim.cmd("colorscheme mine")
 -- plugins
 require "packer".startup(function()
 	use 'wbthomason/packer.nvim'
-	use 'nvim-lua/plenary.nvim'
 
 	-- editor
 	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-	use 'RRethy/vim-illuminate'
-	use 'ibhagwan/fzf-lua'
 	use 'phaazon/hop.nvim'
+	use 'ibhagwan/fzf-lua'
 	use 'declancm/cinnamon.nvim'
-	use 'kyazdani42/nvim-web-devicons'
 	use "lukas-reineke/indent-blankline.nvim"
-	use "tversteeg/registers.nvim"
 
 	-- git
-	use 'tpope/vim-fugitive'
 	use 'lewis6991/gitsigns.nvim'
 
 	-- productivity
@@ -43,16 +38,13 @@ require "nvim-treesitter.configs".setup {
 	}
 }
 
-vim.keymap.set("n", "<C-n>", function() require 'illuminate'.next_reference { wrap = true } end)
-vim.keymap.set("n", "<C-p>", function() require 'illuminate'.next_reference { reverse = true, wrap = true } end)
-
 require 'fzf-lua'.setup {
 	winopts = {
 		border     = 'none',
 		fullscreen = true,
 		preview    = {
 			vertical     = 'up:50%',
-			horizontal   = 'left:50%',
+			horizontal   = 'right:50%',
 			flip_columns = 150,
 			scrollbar    = false,
 		}
@@ -63,7 +55,7 @@ vim.keymap.set("n", "<Space>k", require 'fzf-lua'.files)
 vim.keymap.set("n", "<Space>l", require 'fzf-lua'.live_grep)
 
 require 'hop'.setup {}
-vim.keymap.set("n", "<Space>f", require 'hop'.hint_words)
+vim.keymap.set("n", "f", require 'hop'.hint_char1)
 
 require 'cinnamon'.setup {
 	extra_keymaps = true,
@@ -72,15 +64,15 @@ require 'cinnamon'.setup {
 	default_delay = 1
 }
 
-vim.cmd 'hi IndentBlanklineChar ctermfg=8'
 vim.g.indent_blankline_filetype = { 'json', 'yaml' }
+vim.cmd 'hi IndentBlanklineChar ctermfg=8'
 
 vim.cmd 'hi GitSignsAddInline ctermbg=2 ctermfg=0'
 vim.cmd 'hi GitSignsChangeInline ctermbg=11 ctermfg=0'
 vim.cmd 'hi GitSignsDeleteInline ctermbg=1 ctermfg=0'
-
--- TODO remove this: https://github.com/lewis6991/gitsigns.nvim/issues/582 
+-- TODO remove this: https://github.com/lewis6991/gitsigns.nvim/issues/582
 vim.cmd 'hi link GitSignsDeleteLn DiffDelete'
+
 require 'gitsigns'.setup {
 	on_attach = function(bufnr)
 		local gs = package.loaded.gitsigns
@@ -92,33 +84,26 @@ require 'gitsigns'.setup {
 		end
 
 		-- Navigation
-		map('n', ']c', function()
-			if vim.wo.diff then return ']c' end
+		map('n', ']h', function()
+			if vim.wo.diff then return ']h' end
 			vim.schedule(function() gs.next_hunk() end)
 			return '<Ignore>'
 		end, { expr = true })
 
-		map('n', '[c', function()
-			if vim.wo.diff then return '[c' end
+		map('n', '[h', function()
+			if vim.wo.diff then return '[h' end
 			vim.schedule(function() gs.prev_hunk() end)
 			return '<Ignore>'
 		end, { expr = true })
 
 		-- Actions
-		map({ 'n', 'v' }, '<Space>hs', ':Gitsigns stage_hunk<CR>')
-		map({ 'n', 'v' }, '<Space>hr', ':Gitsigns reset_hunk<CR>')
+		map('n', '<Space>hs', gs.stage_hunk)
+		map('n', '<Space>hr', gs.reset_hunk)
 		map('n', '<Space>hS', gs.stage_buffer)
 		map('n', '<Space>hR', gs.reset_buffer)
-		map('n', '<Space>hu', gs.undo_stage_hunk)
+		map('n', '<Space>hU', gs.reset_buffer_index)
 		map('n', '<Space>hp', gs.preview_hunk)
-		map('n', '<Space>gb', function() gs.blame_line { full = true } end)
-		-- map('n', '<Space>tb', gs.toggle_current_line_blame)
-		map('n', '<Space>gd', gs.diffthis)
-		-- map('n', '<Space>hD', function() gs.diffthis('~') end)
-		-- map('n', '<Space>td', gs.toggle_deleted)
-
-		-- Text object
-		map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+		map('n', '<Space>gb', gs.blame_line)
 	end
 }
 
@@ -142,7 +127,6 @@ require "nvim-lsp-installer".setup({
 	automatic_installation = true,
 })
 
-
 -- key mappings
 for i = 1, 9 do
 	vim.keymap.set("n", "<Space>" .. i, i .. "gt")
@@ -153,17 +137,15 @@ vim.keymap.set("n", "<Space>q", "<Cmd>q!<CR>")
 vim.keymap.set("n", "<Space>Q", "<Cmd>qa!<CR>")
 vim.keymap.set("n", "<Space>w", "<Cmd>w<CR>")
 vim.keymap.set("n", "<Space>W", "<Cmd>wa<CR>")
-vim.keymap.set("i", "<C-n>", "<Plug>(copilot-next)")
-vim.keymap.set("i", "<C-p>", "<Plug>(copilot-previous)")
+vim.keymap.set("i", "]c", "<Plug>(copilot-next)")
+vim.keymap.set("i", "[c", "<Plug>(copilot-previous)")
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<Space>d", function() vim.diagnostic.open_float(nil, { focusable = false }) end, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 
-local on_attach = function(client, bufnr)
-	require 'illuminate'.on_attach(client)
-
+local on_attach = function(_, bufnr)
 	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
 	local aopts = { noremap = true, silent = true, buffer = bufnr }
@@ -173,16 +155,12 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set('n', 'gr', require 'fzf-lua'.lsp_references, aopts)
 	vim.keymap.set('n', 'gt', require 'fzf-lua'.lsp_typedefs, aopts)
 
-	vim.keymap.set('n', 'K', vim.lsp.buf.hover, aopts)
-	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, aopts)
+	vim.keymap.set('n', '<Space>h', vim.lsp.buf.hover, aopts)
+	vim.keymap.set('n', '<Space>s', vim.lsp.buf.signature_help, aopts)
 
 	vim.keymap.set('n', '<Space>r', vim.lsp.buf.rename, aopts)
 	vim.keymap.set('n', '<Space>a', require 'fzf-lua'.lsp_code_actions, aopts)
 	vim.keymap.set('n', '<Space>f', vim.lsp.buf.formatting, aopts)
-
-	vim.keymap.set('n', '<Space>wa', vim.lsp.buf.add_workspace_folder, aopts)
-	vim.keymap.set('n', '<Space>wr', vim.lsp.buf.remove_workspace_folder, aopts)
-	vim.keymap.set('n', '<Space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, aopts)
 end
 
 for _, server in ipairs(servers) do
