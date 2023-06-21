04080c890ba4a17ce6694edb7573558f22ec3db1
許煜廷 <me@hsuyuting.com>
2023-06-21 10:04:22 +0800

    update

--- setup.sh
+++ setup.sh
@@ -7,12 +7,13 @@ ln -s /Users/hsuyuting/config/.profile /Users/hsuyuting/.profile
 ln -s /Users/hsuyuting/config/.gitignore /Users/hsuyuting/.gitignore
 ln -s /Users/hsuyuting/config/.gitconfig /Users/hsuyuting/.gitconfig
 ln -s /Users/hsuyuting/config/.tmux.conf /Users/hsuyuting/.tmux.conf
-
+ln -s /Users/hsuyuting/Google Drive/我的雲端硬碟/a.yaml /Users/hsuyuting/a.yaml
+ln -s /Users/hsuyuting/Google Drive/我的雲端硬碟/notes/ /Users/hsuyuting/notes
 mkdir /Users/hsuyuting/.config
 ln -s /Users/hsuyuting/config/nvim /Users/hsuyuting/.config/nvim
 ln -s /Users/hsuyuting/config/alacritty /Users/hsuyuting/.config/alacritty
 
 touch /Users/hsuyuting/local.sh
 
-echo "/opt/homebrew/bin/bash" >> /etc/shells
-chsh -s /opt/homebrew/bin/bash
+sudo echo "/opt/homebrew/bin/bash" >> /etc/shells
+sudo chsh -s /opt/homebrew/bin/bash
