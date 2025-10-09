3a862c836abcedf65f692dbbcfcc1a87fac81815
許煜廷 <me@hsuyuting.com>
2025-10-09 12:15:51 +0800

    update

--- bin/stretch.sh
+++ bin/stretch.sh
@@ -4,9 +4,12 @@ say "2"
 sleep 1
 say "1"
 sleep 1
-say "脖子往左"
+
+say "Cherry Picker"
 sleep 32
-say "脖子往右"
+say "左側彎"
+sleep 32
+say "右側彎"
 sleep 32
 say "左手胸"
 sleep 32
@@ -16,12 +19,17 @@ say "雙手肩背"
 sleep 32
 say "雙手反肩背"
 sleep 32
+
 say "elephant walk"
 sleep 32
 say "對折"
 sleep 32
+say "連續低壓腿"
+sleep 62
 say "左腳單膝跪"
 sleep 32
 say "右腳單膝跪"
+sleep 32
+say "側劈腿"
 sleep 32
 say "結束"
