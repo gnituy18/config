0b30b7767f86281d9050c1a5f1ce3afe8f4370dd
許煜廷 <me@hsuyuting.com>
2026-05-07 18:36:27 +0900

    update

--- chrome-extension/delete-selection/delete-selection.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('keydown', (e) => {
-  if (e.key === 'd') {
-    const selection = window.getSelection();
-    if (selection.rangeCount > 0) {
-      selection.deleteFromDocument()
-    }
-  }
-});
--- chrome-extension/website-confirm/website-confirm.js
+++
@@ -1,3 +0,0 @@
-if (!confirm('你確定要花時間在這個網站嗎？')) {
-  window.location.href = 'https://grok.com';
-}
